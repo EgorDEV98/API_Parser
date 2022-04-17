fa845e3d1430b07d20dc5091665d9d702d9f300f
EgorDEV98 <solonicyn8@yandex.ru>
2022-04-17 17:31:50 +0300

    Добавьте файлы проекта.

---
+++ API/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Service;
+
+namespace API
+{
+    /// <summary>
+    /// Набор классов для проверки людей
+    /// </summary>
+    public class Person
+    {
+
+    }
+
+    /// <summary>
+    /// Набор классов для проверки организаций
+    /// </summary>
+
+    public static class Company
+    {
+        /*=======================================================================================================================*/
+        /// <summary>
+        /// Предоставляет набор методов класса Bankrot
+        /// </summary>
+        /// <returns>Bankrot</returns>
+        public static Bankrot Bankrot() { return new Bankrot(); }
+
+        /// <summary>
+        /// Предоставляет набор методов класса Bankrot
+        /// </summary>
+        /// <param name="data">Наименование лица, ИНН, номера дела</param>
+        /// <returns>Bankrot</returns>
+        public static Bankrot Bankrot(string data) { return new Bankrot(data); }
+
+        /// <summary>
+        /// Предоставляет набор методов класса Bankrot
+        /// </summary>
+        /// <param name="status">Статус дела</param>
+        /// <param name="region">Регион дела. Обратите внимание, реальный номер региона и regionId отличаются!</param>
+        /// <returns>Bankrot</returns>
+        public static Bankrot Bankrot(Status status, RegionId region) { return new Bankrot(status, region); }
+
+        /// <summary>
+        /// Предоставляет набор методов класса Bankrot
+        /// </summary>
+        /// <param name="data">Наименование лица, ИНН, номера дела</param>
+        /// <param name="status">Статус дела</param>
+        /// <param name="region">Регион дела. Обратите внимание, реальный номер региона и regionId отличаются!</param>
+        /// <returns>Bankrot</returns>
+        public static Bankrot Bankrot(string data, Status status, RegionId region) { return new Bankrot(data, status, region); }
+
+        /*=======================================================================================================================*/
+
+
+        
+    }
+
+
+
+
+
+    /// <summary>
+    /// Набор классов для проверки автомобилей
+    /// </summary>
+
+    public class Auto
+    {
+        
+    }
+}
---
+++ API/Service/Bankrot.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace API.Service
+{
+
+    /// <summary>
+    /// Регион дела. Обратите внимание, что RegionID могет не совпадать с реальными номерами регионов
+    /// </summary>
+    public enum RegionId
+    {
+        Все = 0,
+        Алтайский_край = 1,
+        Амурская_область = 10,
+        Архангельская_область = 11,
+        Астраханская_область = 12,
+        Белгородская_область = 14,
+        Брянская_область = 15,
+        Владимирская_область = 17,
+        Волгоградская_область = 18,
+        Вологодская_область = 19,
+        Воронежская_область = 20,
+        Москва = 45,
+        Санкт_Петербург = 40,
+        Севастополь = 201,
+        Еврейская_автономная_область = 99,
+        Забайкальский_край = 101,
+        Ивановская_область = 24,
+        Иные_территории, _включая_г_Байконур = 203,
+        Иркутская_область = 25,
+        Кабардино_Балкарская_Республика = 83,
+        Калининградская_область = 27,
+        Калужская_область = 29,
+        Камчатский_край = 30,
+        Карачаево_Черкесская_Республика = 91,
+        Кемеровская_область = 32,
+        Кировская_область = 33,
+        Костромская_область = 34,
+        Краснодарский_край = 3,
+        Красноярский_край = 4,
+        Курганская_область = 37,
+        Курская_область = 38,
+        Ленинградская_область = 41,
+        Липецкая_область = 42,
+        Магаданская_область = 44,
+        Московская_область = 46,
+        Мурманская_область = 47,
+        Ненецкий_автономный_округ = 200,
+        Нижегородская_область = 22,
+        Новгородская_область = 49,
+        Новосибирская_область = 50,
+        Омская_область = 52,
+        Оренбургская_область = 53,
+        Орловская_область = 54,
+        Пензенская_область = 56,
+        Пермский_край = 57,
+        Приморский_край = 5,
+        Псковская_область = 58,
+        Республика_Адыгея = 79,
+        Республика_Алтай = 84,
+        Республика_Башкортостан = 80,
+        Республика_Бурятия = 81,
+        Республика_Дагестан = 82,
+        Республика_Ингушетия = 26,
+        Республика_Калмыкия = 85,
+        Республика_Карелия = 86,
+        Республика_Коми = 87,
+        Республика_Крым = 202,
+        Республика_Марий_Эл = 88,
+        Республика_Мордовия = 89,
+        Республика_Якутия = 98,
+        Республика_Северная_Осетия = 102,
+        Республика_Татарстан = 92,
+        Республика_Тыва = 93,
+        Республика_Хакасия = 93,
+        Ростовская_область = 60,
+        Рязанская_область = 61,
+        Самарская_область = 36,
+        Саратовская_область = 63,
+        Сахалинская_область = 64,
+        Свердловская_область = 65,
+        Смоленская_область = 66,
+        Ставропольский_край = 7,
+        Тамбовская_область = 68,
+        Тверская_область = 28,
+        Томская_область = 69,
+        Тульская_область = 70,
+        Тюменская_область = 71,
+        Удмуртская_Республика = 94,
+        Ульяновская_область = 73,
+        Хабаровский_край = 8,
+        Ханты_Мансийский_автономный_округ = 103,
+        Челябинская_область = 75,
+        Чеченская_Республика = 96,
+        Чувашская_Республика = 97,
+        Чукотский_автономный_округ = 77,
+        Ямало_Ненецкий_автономный_округ = 104,
+        Ярославская_область = 78
+    }
+
+    /// <summary>
+    /// Статус дела
+    /// </summary>
+    public enum Status
+    {
+        Все = 0,
+        В_работе = 1,
+        Завершено = 2
+    }
+
+
+
+    public partial class Bankrot
+    {
+        private WebClient wk;
+        private const string baseUrl = "https://bankrot.fedresurs.ru/backend/cmpbankrupts";
+
+        public Status status = 0;
+        public RegionId regionId = 0;
+
+        public string data { get; set; }
+
+
+        public Bankrot() { }
+        public Bankrot(string data)
+        {
+            this.data = data;
+        }
+        public Bankrot(Status status, RegionId regionId)
+        {
+            this.status = status;
+            this.regionId = regionId;
+        }
+        public Bankrot(string data, Status status, RegionId regionId) : this(data)
+        {
+            this.status = status;
+            this.regionId = regionId;
+        }
+
+
+        public string GetJSON()
+        {
+            var collection = new NameValueCollection();
+            if (!string.IsNullOrEmpty(data)) collection.Add("searchString", data);
+            collection.Add("isActiveLegalCase", enumStatusToString(status));
+            if (regionId != 0) collection.Add("regionId", enumRegionToString(regionId));
+            collection.Add("limit", "15");
+            collection.Add("offset", "0");
+
+            string queryString = toQueryString(collection);
+
+
+            wk = new WebClient();
+            wk.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36");
+            wk.Headers.Add(HttpRequestHeader.Referer, baseUrl + queryString);
+            wk.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip");
+
+            var responseStream = new GZipStream(wk.OpenRead(baseUrl + queryString), CompressionMode.Decompress);
+            var reader = new StreamReader(responseStream);
+            var textResponse = reader.ReadToEnd();
+
+            return textResponse;
+        }
+
+        private string toQueryString(NameValueCollection nvc)
+        {
+            var array = (
+                from key in nvc.AllKeys
+                from value in nvc.GetValues(key)
+                select string.Format(
+            "{0}={1}",
+            HttpUtility.UrlEncode(key),
+            HttpUtility.UrlEncode(value))
+                ).ToArray();
+            return "?" + string.Join("&", array);
+        }
+        private string enumStatusToString(Status status)
+        {
+            switch ((int)status)
+            {
+                case 1: return "true";
+                case 2: return "false";
+                default: return "null";
+            }
+        }
+        private string enumRegionToString(RegionId regionId)
+        {
+            if ((int)regionId == 0)
+                return "";
+            return "&regionId=" + (int)regionId;
+        }
+    }
+}
